c167cd80e9b82bf45f80a2dd2e5e697f7118b0c7
trendyjosh <trendyjosh@gmail.com>
2024-05-08 10:17:11 +0100

    Use forked repo for css

--- src/glowCookies.js
+++ src/glowCookies.js
@@ -31,7 +31,7 @@ class GlowCookies {
   addCss() {
     const stylesheet = document.createElement("link");
     stylesheet.setAttribute("rel", "stylesheet");
-    stylesheet.setAttribute("href", `https://cdn.jsdelivr.net/gh/manucaralmo/GlowCookies@3.1.3/src/glowCookies.min.css`);
+    stylesheet.setAttribute("href", `https://cdn.jsdelivr.net/gh/WebscriptDesigns/GlowCookies/glowCookies.min.css`);
     document.head.appendChild(stylesheet);
   }
 
